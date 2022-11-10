1ed7782164f385473a5f9fa30aa30e21f2c48d19
ameliaovia <92940263+ameliaovia@users.noreply.github.com>
2022-11-10 09:51:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+function turn_right () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
+    control.waitMicros(20000)
+}
+input.onButtonPressed(Button.A, function () {
+    forward()
+})
+function backwards () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
+    control.waitMicros(20000)
+}
+input.onButtonPressed(Button.B, function () {
+    backwards()
+})
+function forward () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
+    control.waitMicros(20000)
+}
+function turn_left () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
+    control.waitMicros(20000)
+}
+function sensor () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+}
+let distance = 0
+basic.showIcon(IconNames.Skull)
+basic.forever(function () {
+    sensor()
+    if (distance < 20) {
+    	
+    } else {
+        forward()
+    }
+})
